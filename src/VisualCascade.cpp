019885837113374203d8983a9ff920347d0d7094
LaunchGemini <metabaseguy@proton.me>
2023-10-31 12:38:03 +0000

    artwork

--- src/VisualCascade.cpp
+++ src/VisualCascade.cpp
@@ -1,1 +1,12 @@
-#include "Visua
+#include "VisualCascade.hpp"
+
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include "objdetect/VisualHaar.hpp"
+
+using namespace cv;
+using n
