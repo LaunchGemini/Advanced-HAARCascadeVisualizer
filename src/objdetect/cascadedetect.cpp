e7f9579a958c6209012e0041c42a5dff22ba2719
LaunchGemini <metabaseguy@proton.me>
2024-01-10 06:56:18 +0000

    docblocks

--- src/objdetect/cascadedetect.cpp
+++ src/objdetect/cascadedetect.cpp
@@ -103,3 +103,236 @@ void groupRectangles(std::vector<Rect>& rectList, int groupThreshold, double eps
             }
             else if( ( (*weights)[i] == rejectLevels[cls] ) && ( (*levelWeights)[i] > rejectWeights[cls] ) )
                 rejectWeights[cls] = (*levelWeights)[i];
+        }
+    }
+    else
+        useDefaultWeights = true;
+
+    for( i = 0; i < nclasses; i++ )
+    {
+        Rect r = rrects[i];
+        float s = 1.f/rweights[i];
+        rrects[i] = Rect(saturate_cast<int>(r.x*s),
+             saturate_cast<int>(r.y*s),
+             saturate_cast<int>(r.width*s),
+             saturate_cast<int>(r.height*s));
+    }
+
+    rectList.clear();
+    if( weights )
+        weights->clear();
+    if( levelWeights )
+        levelWeights->clear();
+
+    for( i = 0; i < nclasses; i++ )
+    {
+        Rect r1 = rrects[i];
+        int n1 = rweights[i];
+        double w1 = rejectWeights[i];
+        int l1 = rejectLevels[i];
+
+        // filter out rectangles which don't have enough similar rectangles
+        if( n1 <= groupThreshold )
+            continue;
+        // filter out small face rectangles inside large rectangles
+        for( j = 0; j < nclasses; j++ )
+        {
+            int n2 = rweights[j];
+
+            if( j == i || n2 <= groupThreshold )
+                continue;
+            Rect r2 = rrects[j];
+
+            int dx = saturate_cast<int>( r2.width * eps );
+            int dy = saturate_cast<int>( r2.height * eps );
+
+            if( i != j &&
+                r1.x >= r2.x - dx &&
+                r1.y >= r2.y - dy &&
+                r1.x + r1.width <= r2.x + r2.width + dx &&
+                r1.y + r1.height <= r2.y + r2.height + dy &&
+                (n2 > std::max(3, n1) || n1 < 3) )
+                break;
+        }
+
+        if( j == nclasses )
+        {
+            rectList.push_back(r1);
+            if( weights )
+                weights->push_back(useDefaultWeights ? n1 : l1);
+            if( levelWeights )
+                levelWeights->push_back(w1);
+        }
+    }
+}
+
+class MeanshiftGrouping
+{
+public:
+    MeanshiftGrouping(const Point3d& densKer, const std::vector<Point3d>& posV,
+        const std::vector<double>& wV, double eps, int maxIter = 20)
+    {
+        densityKernel = densKer;
+        weightsV = wV;
+        positionsV = posV;
+        positionsCount = (int)posV.size();
+        meanshiftV.resize(positionsCount);
+        distanceV.resize(positionsCount);
+        iterMax = maxIter;
+        modeEps = eps;
+
+        for (unsigned i = 0; i<positionsV.size(); i++)
+        {
+            meanshiftV[i] = getNewValue(positionsV[i]);
+            distanceV[i] = moveToMode(meanshiftV[i]);
+            meanshiftV[i] -= positionsV[i];
+        }
+    }
+
+    void getModes(std::vector<Point3d>& modesV, std::vector<double>& resWeightsV, const double eps)
+    {
+        for (size_t i=0; i <distanceV.size(); i++)
+        {
+            bool is_found = false;
+            for(size_t j=0; j<modesV.size(); j++)
+            {
+                if ( getDistance(distanceV[i], modesV[j]) < eps)
+                {
+                    is_found=true;
+                    break;
+                }
+            }
+            if (!is_found)
+            {
+                modesV.push_back(distanceV[i]);
+            }
+        }
+
+        resWeightsV.resize(modesV.size());
+
+        for (size_t i=0; i<modesV.size(); i++)
+        {
+            resWeightsV[i] = getResultWeight(modesV[i]);
+        }
+    }
+
+protected:
+    std::vector<Point3d> positionsV;
+    std::vector<double> weightsV;
+
+    Point3d densityKernel;
+    int positionsCount;
+
+    std::vector<Point3d> meanshiftV;
+    std::vector<Point3d> distanceV;
+    int iterMax;
+    double modeEps;
+
+    Point3d getNewValue(const Point3d& inPt) const
+    {
+        Point3d resPoint(.0);
+        Point3d ratPoint(.0);
+        for (size_t i=0; i<positionsV.size(); i++)
+        {
+            Point3d aPt= positionsV[i];
+            Point3d bPt = inPt;
+            Point3d sPt = densityKernel;
+
+            sPt.x *= std::exp(aPt.z);
+            sPt.y *= std::exp(aPt.z);
+
+            aPt.x /= sPt.x;
+            aPt.y /= sPt.y;
+            aPt.z /= sPt.z;
+
+            bPt.x /= sPt.x;
+            bPt.y /= sPt.y;
+            bPt.z /= sPt.z;
+
+            double w = (weightsV[i])*std::exp(-((aPt-bPt).dot(aPt-bPt))/2)/std::sqrt(sPt.dot(Point3d(1,1,1)));
+
+            resPoint += w*aPt;
+
+            ratPoint.x += w/sPt.x;
+            ratPoint.y += w/sPt.y;
+            ratPoint.z += w/sPt.z;
+        }
+        resPoint.x /= ratPoint.x;
+        resPoint.y /= ratPoint.y;
+        resPoint.z /= ratPoint.z;
+        return resPoint;
+    }
+
+    double getResultWeight(const Point3d& inPt) const
+    {
+        double sumW=0;
+        for (size_t i=0; i<positionsV.size(); i++)
+        {
+            Point3d aPt = positionsV[i];
+            Point3d sPt = densityKernel;
+
+            sPt.x *= std::exp(aPt.z);
+            sPt.y *= std::exp(aPt.z);
+
+            aPt -= inPt;
+
+            aPt.x /= sPt.x;
+            aPt.y /= sPt.y;
+            aPt.z /= sPt.z;
+
+            sumW+=(weightsV[i])*std::exp(-(aPt.dot(aPt))/2)/std::sqrt(sPt.dot(Point3d(1,1,1)));
+        }
+        return sumW;
+    }
+
+    Point3d moveToMode(Point3d aPt) const
+    {
+        Point3d bPt;
+        for (int i = 0; i<iterMax; i++)
+        {
+            bPt = aPt;
+            aPt = getNewValue(bPt);
+            if ( getDistance(aPt, bPt) <= modeEps )
+            {
+                break;
+            }
+        }
+        return aPt;
+    }
+
+    double getDistance(Point3d p1, Point3d p2) const
+    {
+        Point3d ns = densityKernel;
+        ns.x *= std::exp(p2.z);
+        ns.y *= std::exp(p2.z);
+        p2 -= p1;
+        p2.x /= ns.x;
+        p2.y /= ns.y;
+        p2.z /= ns.z;
+        return p2.dot(p2);
+    }
+};
+//new grouping function with using meanshift
+static void groupRectangles_meanshift(std::vector<Rect>& rectList, double detectThreshold, std::vector<double>* foundWeights,
+                                      std::vector<double>& scales, Size winDetSize)
+{
+    int detectionCount = (int)rectList.size();
+    std::vector<Point3d> hits(detectionCount), resultHits;
+    std::vector<double> hitWeights(detectionCount), resultWeights;
+    Point2d hitCenter;
+
+    for (int i=0; i < detectionCount; i++)
+    {
+        hitWeights[i] = (*foundWeights)[i];
+        hitCenter = (rectList[i].tl() + rectList[i].br())*(0.5); //center of rectangles
+        hits[i] = Point3d(hitCenter.x, hitCenter.y, std::log(scales[i]));
+    }
+
+    rectList.clear();
+    if (foundWeights)
+        foundWeights->clear();
+
+    double logZ = std::log(1.3);
+    Point3d smothing(8, 16, logZ);
+
+    MeanshiftGrouping msGrouping(smothing, hits, hitWeights, 1e-5, 100);
